61d2cfcaa1723fc52642d8a866c4896f887d8dcd
nam <listenme1@naver.com>
2018-10-24 19:05:36 +0900

    postsevice 준비중

---
+++ src/main/java/com/listenme/springwebexample/service/PostsService.java
@@ -0,0 +1,4 @@
+package com.listenme.springwebexample.service;
+
+public class PostsService {
+}
---
+++ src/main/java/com/listenme/springwebexample/web/WebController.java
@@ -0,0 +1,14 @@
+package com.listenme.springwebexample.web;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@AllArgsConstructor
+public class WebController {
+    @GetMapping("/")
+    public String main() {
+        return "main";
+    }
+}
